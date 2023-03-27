fab71a60a0b634c1325394e1e7b779af1839fc39
nakano <nakano@fine.cs.kobe-u.ac.jp>
2023-03-27 19:16:19 +0900

    0.75

--- js/main.js
+++ js/main.js
@@ -44,11 +44,11 @@ function onAssetsLoaded(loader, res) {
     char = new PIXI.spine.Spine(res.char.spineData);
 
     // Scaler
-    char.scale.x = char.scale.y = Math.max(screen.height/char.spineData.height,screen.width/char.spineData.width);
+    char.scale.x = char.scale.y = Math.max(screen.height/char.spineData.height, screen.width/char.spineData.width);
 
     // Centerize
     char.x = screen.width / 2;
-    char.y = screen.height - char.spineData.height*char.scale.y/8;
+    char.y = screen.height * 0.75;
 
     // //Play Animation
     char.state.addAnimation(0,"Start_Idle_01",false)
